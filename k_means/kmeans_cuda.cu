2962ce5e44f1fb2971f3ea619130843455747a0a
kanika127 <41696750+kanika127@users.noreply.github.com>
2022-12-05 18:49:10 -0500

    Add files via upload

--- k_means/kmeans_cuda.cu
+++ k_means/kmeans_cuda.cu
@@ -1,21 +1,22 @@
 #include <cstdio>
 #include <random>
-#include <iostream>	///
+#include <iostream>
 #include <cstdlib>
+#include <sys/time.h>
 #include "utils.h"
 
-using namespace std;	///
-#define square(X) X*X
+using namespace std;
+
 #define THREADS_PER_BLOCK 512
 
 /*****************************************************************/
 
+void get_cluster_colors(int cluster_colors[], int, int);
 /*** Kernel Definitions ***/
-__global__ void find_cluster(int, int, Pixel*, const Cluster* __restrict__);
-__global__ void recenter1(int, int, Pixel*, uint4*);
-__global__ void recenter2(Cluster*, uint4*, bool*);
+__global__ void clustering(int, int, Point2d*, const Cluster* __restrict__);
+__global__ void recenter_sum(int, int, Point2d*, uint4*);
+__global__ void convergence_check(Cluster*, uint4*, bool*);
 /**** end of the kernel declaration ***/
-void rgb_color_code(int rgb[], int, int);
 
 /*****************************************************************/
 
@@ -23,37 +24,39 @@ int main(int argc, char * argv[]) {
 	
 	srand((unsigned) time(NULL));
 	if(argc != 4){
-        fprintf(stderr, "usage: kmeans_sequential <IN_PATH> <OUT_PATH> <K_CLUSTERS> \n");
+		cout<<"usage: kmeans_cuda <K_CLUSTERS> <INPUT_FILE_PATH> <OUTPUT_FILE_PATH>"<<endl;
         exit(1);
     }
 
-	const char *inPath, *outPath;
-	inPath = argv[1]; outPath = argv[2];
-	int K_clusters = atoi(argv[3]);
-	
-	unsigned char* image;
-
-	int idx = 0;
-	int img_height = 600, img_width = 800;
-	int total_pixels = img_height * img_width;
+	const char *in_file, *out_file;
+	in_file = argv[2]; out_file = argv[3];
+	int K_clusters = atoi(argv[1]);
 	int total_blobs = 10;
+	int points_per_blob = 5000;
+	
+	int idx = 0, img_height = 600, img_width = 800;
+	int total_img_points = img_height * img_width;
 	// int total_blobs = 2 * K_clusters;
 	int blob_radius = 150;
-	int blob_centres[100][2]; // change
-	int pixels_per_blob = 5000;
-	int n_pixels = total_blobs * pixels_per_blob;
-	Pixel* pixels = (Pixel*)calloc(n_pixels, sizeof(Pixel));
-	unsigned char* in_image = (unsigned char*)calloc(total_pixels*3, sizeof(unsigned char*));
+	int blob_centres[100][2];
+	int total_blob_points = total_blobs * points_per_blob;
+	Point2d* points = (Point2d*)calloc(total_blob_points, sizeof(Point2d));
+	unsigned char* in_image = (unsigned char*)calloc(total_img_points*3, sizeof(unsigned char*));
+	unsigned char* out_image = (unsigned char*)calloc(total_img_points*3, sizeof(unsigned char*));
 
-	while(idx < total_pixels){
+	while(idx < total_img_points){
 		in_image[3*idx] = 255;
 		in_image[3*idx+1] = 255;
 		in_image[3*idx+2] = 255;
+		out_image[3*idx] = 255;
+		out_image[3*idx+1] = 255;
+		out_image[3*idx+2] = 255;
 		idx++;
 	}
-	int rgb[K_clusters][3];
+
+	int cluster_colors[K_clusters][3];
 	for (int i=0; i<K_clusters; i++) {
-		rgb_color_code(rgb[i], K_clusters, i);
+		get_cluster_colors(cluster_colors[i], K_clusters, i);
 	}
 	
 	for (int i=0; i<total_blobs; i++) {
@@ -62,13 +65,13 @@ int main(int argc, char * argv[]) {
 	}
 	int p_idx = 0;
 	for (int i=0; i<total_blobs; i++) {
-		for (int j=0; j<pixels_per_blob; j++) {
+		for (int j=0; j<points_per_blob; j++) {
 			int x = blob_centres[i][0] + rand()%blob_radius;
 			int y = blob_centres[i][1] + rand()%blob_radius;
 			int pos = img_width*y + x;
-			pixels[p_idx].x = x;
-			pixels[p_idx].y = y;
-			pixels[p_idx].cluster = -1;
+			points[p_idx].x = x;
+			points[p_idx].y = y;
+			points[p_idx].cluster = -1;
 	 		in_image[3*pos] = 0;
 			in_image[3*pos+1] = 0;
 			in_image[3*pos+2] = 0;
@@ -76,170 +79,160 @@ int main(int argc, char * argv[]) {
 		}
 	}
 
-	if ((write_png(inPath, in_image, img_height, img_width, 3)) != 0) {
-		cout<<"fail to write input png file"<<endl;
-		// printf("fail to write input png file\n");
+	if ((write_png(in_file, in_image, img_height, img_width, 3)) != 0) {
+		cout<<"Failed to write the input .png file"<<endl;
 		exit(1);
 	}
-
-	int i=0;
 
 	Cluster* clusters = (Cluster*)calloc(K_clusters, sizeof(Cluster));
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> uniform(0, n_pixels - 1);
-	i=0;
+	std::uniform_int_distribution<> uniform(0, total_blob_points - 1);
 	
-	//Initialize Cluster and Assign a Random Pixel to the Cluster
+	struct timeval tv1, tv2;	
+    struct timezone tz;	
+    double elapsed;
+	int i=0;
+    gettimeofday(&tv1, &tz);	
+
+	//Initialize clusters by assigning a random 2D Point to the cluster
 	while (i<K_clusters) {
-		Pixel *pixel = &pixels[uniform(gen)];
-		clusters[i++] = Cluster(pixel->x, pixel->y, 0, 0, (int*)calloc(n_pixels, sizeof(int)));
-		cout<<"cluster centres: "<<pixel->x<<" .. "<<pixel->y<<endl; ///
+		Point2d *point = &points[uniform(gen)];
+		clusters[i++] = Cluster(point->x, point->y, 0, (int*)calloc(total_blob_points, sizeof(int)));
 	}
- 	cout<<endl;	///
 
-	//Define Blocks and Threads per block
-    dim3 numBlocks((n_pixels + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK);
-    dim3 threadsPerBlock(THREADS_PER_BLOCK);
+    dim3 total_blocks((total_blob_points + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK);
+    dim3 threads_per_block(THREADS_PER_BLOCK);
 
-	// allocate device memory
-	Pixel* d_pixels;
+	//Allocate device memory
+	Point2d* d_points;
 	Cluster* d_cluster;
-	size_t sz_pixel= sizeof(Pixel), sz_cluster = sizeof(Cluster);
-	uint4* d_sum;	//To store sum and count
+	size_t point_size = sizeof(Point2d), cluster_size = sizeof(Cluster);
 	bool* d_converged;
+	uint4* d_sum;
 
-	cudaMalloc((void**)&d_pixels, n_pixels * sz_pixel);
-	cudaMalloc((void**)&d_cluster, K_clusters * sz_cluster);
-	cudaMalloc((void**)&d_sum, sizeof(uint4));
+	cudaMalloc((void**)&d_points, total_blob_points * point_size);
+	cudaMalloc((void**)&d_cluster, K_clusters * cluster_size);
 	cudaMalloc((void**)&d_converged, sizeof(bool));
+	cudaMalloc((void**)&d_sum, sizeof(uint4));
 
-	if(!d_pixels && !d_cluster && !d_sum && !d_converged){
-        printf("cannot allocate array\n");
+	if(!d_points && !d_cluster && !d_sum && !d_converged){
+        cout<<"array cannot be allocated"<<endl;
         exit(1);
     }
 
-	cudaMemcpy(d_pixels, pixels, n_pixels * sz_pixel, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_cluster, clusters, K_clusters * sz_cluster, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_cluster, clusters, K_clusters * cluster_size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_points, points, total_blob_points * point_size, cudaMemcpyHostToDevice);
 
-
-	bool thread_converged = true;
-	bool converged;
-	int itr = 0;
-	do{
-		itr++;
-		find_cluster<<<numBlocks , threadsPerBlock>>>(n_pixels, K_clusters, d_pixels, d_cluster);
-		thread_converged = true;
-		for (int i = 0; i < K_clusters; ++i) {
+	bool all_converged = true, converged;
+	do {
+		clustering<<<total_blocks , threads_per_block>>>(total_blob_points, K_clusters, d_points, d_cluster);
+		all_converged = true;
+		for (int i = 0; i < K_clusters; i++) {
 			cudaMemset(d_sum, 0, 4 * sizeof(int));
-
-			recenter1<<<numBlocks , threadsPerBlock>>>(n_pixels, i, d_pixels, d_sum);
-			recenter2<<<1, 1>>>(&d_cluster[i], d_sum, d_converged);
-
+			recenter_sum<<<total_blocks , threads_per_block>>>(total_blob_points, i, d_points, d_sum);
+			convergence_check<<<1, 1>>>(&d_cluster[i], d_sum, d_converged);
 			cudaMemcpy(&converged, d_converged, sizeof(bool), cudaMemcpyDeviceToHost);
-
-			thread_converged &= converged;
+			all_converged &= converged;
 		}
-	} while (!thread_converged || itr > 50000);
+	} while (!all_converged); // Loop Until Convergence of Centroids
 
-	// copy device memory back to host
-	cudaMemcpy(pixels, d_pixels, n_pixels * sz_pixel, cudaMemcpyDeviceToHost);
-	cudaMemcpy(clusters, d_cluster, K_clusters * sz_cluster, cudaMemcpyDeviceToHost);
+	// copy device memory to host
+	cudaMemcpy(points, d_points, total_blob_points * point_size, cudaMemcpyDeviceToHost);
+	cudaMemcpy(clusters, d_cluster, K_clusters * cluster_size, cudaMemcpyDeviceToHost);
 
+	// Print the final cluster centre points and the color associated to each cluster
+	/*
 	for (int i=0; i<K_clusters; i++) {
-		// cout<<"rgb: "<<i<<rgb[i][0]<<" .. "<<rgb[i][1]<<" .. "<<rgb[i][2]<<endl; ///
-		cout<<"final cluster "<<i+1<<": "<<(int)clusters[i].x<<" "<<(int)clusters[i].y<<" "<<(int)clusters[i].z<<endl<<endl;	///
+		cout<<"cluster_rgb_colors: "<<i+1<<cluster_colors[i][0]<<" .. "<<cluster_colors[i][1]<<" .. "<<cluster_colors[i][2]<<endl; ///
+		cout<<"final cluster centre-point"<<i+1<<": "<<(int)clusters[i].x<<" "<<(int)clusters[i].y<<endl;	///
 	}
+	*/
 
 	// free device memory
-	cudaFree(d_pixels);
 	cudaFree(d_cluster);
+	cudaFree(d_points);
 	cudaFree(d_sum);
 
+ 	gettimeofday(&tv2, &tz);	
+    elapsed = (double) (tv2.tv_sec-tv1.tv_sec) + (double) (tv2.tv_usec-tv1.tv_usec) * 1.e-6;	
+    printf("elapsed time = %f seconds.\n", elapsed);
+
 	idx = 0;
-	unsigned char* out_image = (unsigned char*)calloc(total_pixels*3, sizeof(unsigned char*));
-	while(idx < total_pixels){
-		out_image[3*idx] = 255;
-		out_image[3*idx+1] = 255;
-		out_image[3*idx+2] = 255;
-		idx++;
-	}
-	idx = 0;
-	while(idx < n_pixels){
-		int cluster_idx = pixels[idx].cluster;
-		int pos = img_width*pixels[idx].y + pixels[idx].x;
-		out_image[3*pos] = rgb[cluster_idx][0];
-		out_image[3*pos+1] = rgb[cluster_idx][1];
-		out_image[3*pos+2] = rgb[cluster_idx][2];
+	while(idx < total_blob_points){
+		int cluster_idx = points[idx].cluster;
+		int pos = img_width*points[idx].y + points[idx].x;
+		out_image[3*pos] = cluster_colors[cluster_idx][0];
+		out_image[3*pos+1] = cluster_colors[cluster_idx][1];
+		out_image[3*pos+2] = cluster_colors[cluster_idx][2];
 		idx++;
 	}
 
-	if ((write_png(outPath, out_image, img_height, img_width, 3)) != 0) {
-		printf("fail to write output png file\n");
+	if ((write_png(out_file, out_image, img_height, img_width, 3)) != 0) {
+		cout<<"Failed to write the output .png file"<<endl;
 		exit(1);
 	}
 
 	delete[] clusters;
-	delete[] pixels;
+	delete[] points;
 
 	return 0;
 }
 
-void rgb_color_code(int rgb[], int K_clusters, int cluster_idx) {
+void get_cluster_colors(int cluster_colors[], int K_clusters, int cluster_idx) {
 	cluster_idx++;
 	int idx = 2;
 	if (K_clusters < 7) {
 	  	for(int power=4; power>0; power/=2) {
-	    	rgb[idx--] = (cluster_idx/power) * 255;
+	    	cluster_colors[idx--] = (cluster_idx/power) * 255;
 	    	cluster_idx = cluster_idx%power;
 		}
 	} else {
 	  	for(int i=0;i<3;i++) {
-	    	rgb[i] = (rand()%K_clusters) * (255/K_clusters);
+	    	cluster_colors[i] = (rand()%K_clusters) * (255/K_clusters);
 	  	}
 	}
 }
 
-__global__ void find_cluster(int total_pixels, int K_clusters, Pixel* pixels, const Cluster* __restrict__ clusters) {
+__global__ void clustering(int total_img_points, int K_clusters, Point2d* points, const Cluster* __restrict__ clusters) {
 	int idx = threadIdx.x + blockDim.x * blockIdx.x;
-	if (idx < total_pixels){
-		Pixel pixel = pixels[idx];
-		int min = INT_MAX, min_cluster, dist, j=0;
+	if (idx < total_img_points){
+		Point2d point = points[idx];
+		int min = INT_MAX, min_cluster, dist, i=0;
 
-		while(j<K_clusters){
-			dist = square((pixel.x - clusters[j].x))+ square((pixel.y - clusters[j].y));
+		while (i < K_clusters){
+			dist = square((point.x - clusters[i].x))+ square((point.y - clusters[i].y));
 			if (dist < min) {
 				min = dist;
-				min_cluster = j;
+				min_cluster = i;
 			}
-			j++;
+			i++;
 		}
-		pixels[idx].cluster = min_cluster;
+		points[idx].cluster = min_cluster;
 	}
 }
 
-__global__ void recenter1(int total_pixels, int cluster, Pixel* pixels, uint4* sumc) {
+__global__ void recenter_sum(int total_img_points, int cluster, Point2d* points, uint4* sumc) {
 	int idx = threadIdx.x + blockDim.x * blockIdx.x;
-	if (idx < total_pixels){
-		Pixel pixel = pixels[idx];
-		if (pixel.cluster == cluster) {
-			atomicAdd(&sumc->x, pixel.x);
-			atomicAdd(&sumc->y, pixel.y);
+	if (idx < total_img_points){
+		Point2d point = points[idx];
+		if (point.cluster == cluster) {
+			atomicAdd(&sumc->x, point.x);
+			atomicAdd(&sumc->y, point.y);
 			atomicAdd(&sumc->w, 1);
 		}
 	}
 
 }
 
-__global__ void recenter2(Cluster* cluster, uint4* sumc, bool* converged) {
-	uint32_t points = sumc->w ;
+__global__ void convergence_check(Cluster* cluster, uint4* sumc, bool* converged) {
+	uint32_t total_cluster_points = sumc->w ;
 	*converged = false;
-	if (points > 0) {
+	if (total_cluster_points > 0) {
 		Cluster copy = *cluster;
-		cluster->x = (sumc->x) / (points);
-		cluster->y = (sumc->y) / (points);
-		if (cluster->x == copy.x && 
-			cluster->y == copy.y)
+		cluster->x = (sumc->x)/total_cluster_points;
+		cluster->y = (sumc->y)/total_cluster_points;
+		if (cluster->x == copy.x && cluster->y == copy.y)
 			*converged=true;
 	}
 }
--- k_means/utils.h
+++ k_means/utils.h
@@ -1,95 +1,25 @@
-#include <stdint.h>
 #include <png.h>
+#include <stdint.h>
 
-// K-Means pixel point color for an RGB image
-struct Pixel { // Point2D
+#define square(x) x*x
+
+// K-Means 2D point in an image
+struct Point2d { // Point2D
 	uint32_t x;   
 	uint32_t y;   
 	int cluster=0; // the cluster id
+	Point2d(uint32_t x, uint32_t y, int c): x(x), y(y), cluster(c){}
 };
 
-struct Point3d {
-	uint32_t x;
-	uint32_t y;
-	uint32_t z;
-	Point3d(uint32_t x, uint32_t y, uint32_t z):x(x), y(y), z(z){}
-};
-
-#define square(X) X*X
-
-// K-Means cluster centeral points
+// K-Means cluster central points
 struct Cluster {
 	uint32_t x;
 	uint32_t y;
-	uint32_t z;
 	size_t size; // the number of points in the cluster
-	int* pixels; // the point ids in the cluster
-	Cluster(uint32_t x, uint32_t y, uint32_t z, size_t size, int* pixels):x(x), y(y), z(z), size(size), pixels(pixels){}
-	Cluster():x(0), y(0), z(0), size(0){}
+	int* points; // the point ids in the cluster
+	Cluster(uint32_t x, uint32_t y, size_t size, int* points):x(x), y(y), size(size), points(points){}
+	Cluster():x(0), y(0), size(0){}
 };
-
-
-int read_png(const char* filename, unsigned char** image, unsigned int& height, unsigned int& width, unsigned int& channels) {
-	unsigned char sig[8];
-
-	FILE* fp = fopen(filename, "rb");
-	if (!fp) {
-		printf("Unable to read the input PNG File \n");
-		return 1;
-	}
-
-	fread(sig, 1, 8, fp);
-	png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-	png_infop info_ptr = png_create_info_struct(png_ptr);
-
-	png_init_io(png_ptr, fp);
-	png_set_sig_bytes(png_ptr, 8);
-	png_read_info(png_ptr, info_ptr);
-
- 	width = png_get_image_width(png_ptr, info_ptr);
-	height = png_get_image_height(png_ptr, info_ptr);
-	png_uint_32 color_type = png_get_color_type(png_ptr, info_ptr);
-	png_uint_32 bit_depth = png_get_bit_depth(png_ptr, info_ptr);
-
-	if (color_type != PNG_COLOR_TYPE_RGB) {
-		// our k-means implementation only support png image with color type RGB (3 channels)
-		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-		return 2;
-	}
-
-	if (bit_depth != 8) {
-		// our k-means implementation only support png image with bit depth 8
-		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-		return 2;
-	}
-
-	channels = png_get_channels(png_ptr, info_ptr);
-	if (channels != 3) {
-		// our k-means implementation only support png image with channels equals 3
-		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-		return 2;
-	}
-
-	size_t row_bytes = width * channels;
-
-	*image = (unsigned char*)calloc(height, row_bytes);
-	if ((*image) == NULL) {
-		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-		return 3;
-	}
-
-	png_bytep* image_ptr = (png_bytep*)calloc(height, sizeof(png_bytep));
-	for (unsigned int i = 0; i < height; ++i) {
-		image_ptr[i] = (*image) + i * row_bytes;
-	}
-
-	png_read_image(png_ptr, image_ptr);
-	png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-
-	free(image_ptr);
-	fclose(fp);
-	return 0;
-}
 
 int write_png(const char* filename, unsigned char* image, unsigned int height, unsigned int width, unsigned int channels) {
 	FILE* fp = fopen(filename, "wb");
@@ -114,7 +44,7 @@ int write_png(const char* filename, unsigned char* image, unsigned int height, u
 	png_bytep* image_ptr = (png_bytep*)calloc(height, sizeof(png_bytep));
 	size_t row_bytes = width * channels;
 
-	for (int i = 0; i < height; ++i) {
+	for (int i = 0; i < height; i++) {
 		image_ptr[i] = image + i * row_bytes;
 	}
 

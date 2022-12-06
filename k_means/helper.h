ea98be376e8ca908c04b6243b20aae4da8b6876f
kanika127 <kanika127@gmail.com>
2022-12-05 23:48:18 -0500

    input image added and cuda code upfdated

--- k_means/helper.h
+++ k_means/helper.h
@@ -21,6 +21,68 @@ struct Cluster {
 	Cluster(uint32_t x, uint32_t y, size_t size, int* points):x(x), y(y), size(size), points(points){}
 };
 
+bool read_png(const char* filename, unsigned char** image, unsigned int& height, unsigned int& width, unsigned int& channels) {
+	unsigned char sig[8];
+
+	FILE* fp = fopen(filename, "rb");
+	if (!fp) {
+		printf("Unable to read the input PNG File \n");
+		return false;
+	}
+
+	fread(sig, 1, 8, fp);
+	png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	png_infop info_ptr = png_create_info_struct(png_ptr);
+
+	png_init_io(png_ptr, fp);
+	png_set_sig_bytes(png_ptr, 8);
+	png_read_info(png_ptr, info_ptr);
+
+ 	width = png_get_image_width(png_ptr, info_ptr);
+	height = png_get_image_height(png_ptr, info_ptr);
+	png_uint_32 color_type = png_get_color_type(png_ptr, info_ptr);
+	png_uint_32 bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+
+	if (color_type != PNG_COLOR_TYPE_RGB) {
+		// our k-means implementation only support png image with color type RGB (3 channels)
+		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+		return false;
+	}
+
+	if (bit_depth != 8) {
+		// our k-means implementation only support png image with bit depth 8
+		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+		return false;
+	}
+
+	channels = png_get_channels(png_ptr, info_ptr);
+	if (channels != 3) {
+		// our k-means implementation only support png image with channels equals 3
+		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+		return false;
+	}
+
+	size_t row_bytes = width * channels;
+
+	*image = (unsigned char*)calloc(height, row_bytes);
+	if ((*image) == NULL) {
+		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+		return false;
+	}
+
+	png_bytep* image_ptr = (png_bytep*)calloc(height, sizeof(png_bytep));
+	for (unsigned int i = 0; i < height; ++i) {
+		image_ptr[i] = (*image) + i * row_bytes;
+	}
+
+	png_read_image(png_ptr, image_ptr);
+	png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+
+	free(image_ptr);
+	fclose(fp);
+	return true;
+}
+
 bool write_png(const char* filename, unsigned char* image, unsigned int height, unsigned int width, unsigned int channels) {
 	FILE* fp = fopen(filename, "wb");
 
@@ -54,4 +116,13 @@ bool write_png(const char* filename, unsigned char* image, unsigned int height, 
 
 	fclose(fp);
 	return true;
+}
+
+int get_total_blob_points(unsigned char* image, unsigned int height, unsigned int width) {
+	int count = 0, total_img_points = height*width;
+	for (int i=0; i<total_img_points; i++) {
+		if (image[3*i] == 0 && image[3*i + 1] == 0 && image[3*i + 2] == 0) 
+			count++;
+	}
+	return count;
 }
